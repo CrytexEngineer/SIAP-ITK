ffd41a465f31ed0e17857167145991bd0032af2d
CrytexEngineer <aqilmaulana1@gmail.com>
2020-03-05 21:05:58 +0800

    Fix Routes

--- routes/web.php
+++ routes/web.php
@@ -17,37 +17,44 @@ Route::get('/', function () {
 
 
 
-Auth::routes();
 
-//Route::get('/diksat/manajemen/akun_mahasiswa', 'DiksatController@index')->name('diksat')->middleware('diksat');
+
 Route::get('/akunmahasiswa/json','ManajemenAkunMahasiswaController@json');
 Route::get('/akunpegawai/json','ManajemenAkunPegawaiController@json');
 
 Route::get('/home','HomeController@index')->name('home')->middleware('home');
 
 Route::get('/superadmin', 'SuperAdminController@index')->name('superadmin')->middleware('superadmin');
-//Route::get('/superadmin/manajemen/akun_mahasiswa', 'DiksatController@index')->name('superadmin')->middleware('diksat');
+Route::get('/superadmin/manajemen/akun_mahasiswa', 'DiksatController@index')->name('superadmin')->middleware('diksat');
 
 Route::get('/admin', 'AdminController@index')->name('admin')->middleware('admin');
-//Route::get('/admin/manajemen/akun_mahasiswa', 'DiksatController@index')->name('admin')->middleware('diksat');
+Route::get('/admin/manajemen/akun_mahasiswa', 'DiksatController@index')->name('admin')->middleware('diksat');
 
 Route::get('/observer', 'ObserverController@index')->name('observer')->middleware('observer');
 
 Route::get('/warek', 'WarekController@index')->name('warek')->middleware('warek');
-//Route::get('/admin/manajemen/akun_mahasiswa', 'DiksatController@index')->name('admin')->middleware('diksat');
+Route::get('/admin/manajemen/akun_mahasiswa', 'DiksatController@index')->name('admin')->middleware('diksat');
 
 Route::get('/kajur', 'KajurController@index')->name('kajur')->middleware('kajur');
 Route::get('/kaprodi', 'KaprodiController@index')->name('kaprodi')->middleware('kaprodi');
 Route::get('/dikjur', 'DikjurController@index')->name('dikjur')->middleware('dikjur');
 
 Route::get('/diksat', 'DiksatController@index')->name('diksat')->middleware('diksat');
-//Route::get('/diksat/manajemen/akun_mahasiswa', 'DiksatController@index')->name('diksat')->middleware('diksat');
+Route::get('/diksat/manajemen/akun_mahasiswa', 'DiksatController@index')->name('diksat')->middleware('diksat');
 
 Route::get('/dosen', 'DosenController@index')->name('dosen')->middleware('dosen');
 
-Route::resource('/register/mahasiswa', 'RegisterMahasiswaController');
-Route::resource('/register/pegawai', 'RegisterPegawaiController');
 
+
+//Testing Routes
+Auth::routes();
+
+
+Route::post('/register/mahasiswa', 'RegisterMahasiswaController@register')->name('register/mahasiswa');;
+Route::get('/register/mahasiswa', 'RegisterMahasiswaController@showRegistrationForm')->name('register/mahasiswa');
+Route::resource('/register/pegawai', 'RegisterPegawaiController');
 Route::resource('/akunmahasiswa', 'ManajemenAkunMahasiswaController');
 Route::resource('/akunpegawai', 'ManajemenAkunPegawaiController');
 
+Route::get('/akunmahasiswa/json','ManajemenAkunMahasiswaController m@json');
+Route::get('/akunpegawai/json','ManajemenAkunPegawaiController@json');
