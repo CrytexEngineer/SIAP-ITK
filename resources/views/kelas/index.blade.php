4d6f223001a85d401cec49c7e858c548e2ce8091
3pointcurve_ <azambinabdullah@gmail.com>
2020-03-15 11:26:53 +0800

    update controller Kelas, Matakuliah & Program Studi

--- resources/views/kelas/index.blade.php
+++ resources/views/kelas/index.blade.php
@@ -20,30 +20,63 @@
                                 {{ session('status_failed') }}
                             </div>
                         @endif
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <table class="table table-bordered">
+                                        <tr>
+                                            <td>Jurusan</td>
+                                            <td></td>
+                                        </tr>
+                                        <tr>
+                                            <td>Semester</td>
+                                            <td></td>
+                                        </tr>
+                                    </table>
+                                </div>
+                                <div class="col-md-8">
+                                    <table class="table table-bordered">
+                                        <tr>
+                                            <th width="70">Kode Kelas</th>
+                                            <th width="100">Mata Kuliah</th>
+                                            <th width="40">Tahun</th>
+                                            <th width="40">Semester</th>
+                                            <th>Nama Kelas</th>
+                                            <th>Daya Tampung</th>
+                                            <th>NIP Pengajar</th>
+                                            <th>Terisi</th>
+                                            <th>Hari</th>
+                                            <th>Jam Mulai</th>
+                                            <th>Jam Usai</th>
+                                            <th>Jadwal Ruangan</th>
+                                            <th>Kode Jurusan</th>
+                                        </tr>
+                                    </table>
+                                </div>
+                            </div>
 
-                        <a href="/manajemen_akun/mahasiswa/create" class="btn btn-success">Input Data Baru</a>
-                        <hr>
+{{--                        <a href="/manajemen_akun/mahasiswa/create" class="btn btn-success">Input Data Baru</a>--}}
+{{--                        <hr>--}}
 
-                        <table class="table table-bordered" id="users-table">
-                            <thead>
-                            <tr>
-                                <th width="70">Kode Kelas</th>
-                                <th width="100">Mata Kuliah</th>
-                                <th width="40">Tahun</th>
-                                <th width="40">Semester</th>
-                                <th>Nama Kelas</th>
-                                <th>Daya Tampung</th>
-                                <th>NIP Pengajar</th>
-                                <th>Terisi</th>
-                                <th>Hari</th>
-                                <th>Jam Mulai</th>
-                                <th>Jam Usai</th>
-                                <th>Jadwal Ruangan</th>
-                                <th>Kode Jurusan</th>
-                                <th width="85">Action</th>
-                            </tr>
-                            </thead>
-                        </table>
+{{--                        <table class="table table-bordered" id="users-table">--}}
+{{--                            <thead>--}}
+{{--                            <tr>--}}
+{{--                                <th width="70">Kode Kelas</th>--}}
+{{--                                <th width="100">Mata Kuliah</th>--}}
+{{--                                <th width="40">Tahun</th>--}}
+{{--                                <th width="40">Semester</th>--}}
+{{--                                <th>Nama Kelas</th>--}}
+{{--                                <th>Daya Tampung</th>--}}
+{{--                                <th>NIP Pengajar</th>--}}
+{{--                                <th>Terisi</th>--}}
+{{--                                <th>Hari</th>--}}
+{{--                                <th>Jam Mulai</th>--}}
+{{--                                <th>Jam Usai</th>--}}
+{{--                                <th>Jadwal Ruangan</th>--}}
+{{--                                <th>Kode Jurusan</th>--}}
+{{--                                <th width="85">Action</th>--}}
+{{--                            </tr>--}}
+{{--                            </thead>--}}
+{{--                        </table>--}}
                     </div>
                 </div>
             </div>
@@ -52,31 +85,31 @@
 @endsection
 
 
-@push('scripts')
-    <script>
-        $(function() {
-            $('#users-table').DataTable({
-                "scrollX": true,
-                processing: true,
-                serverSide: true,
-                ajax: '/kelas/json', //DIGANTI SESUAI CONTROLLER
-                columns: [
-                    { data: 'KE_ID', name: 'KE_ID' },
-                    { data: 'KE_KR_MK_ID', name: 'KE_KR_MK_ID' },
-                    { data: 'KE_Tahun', name: 'KE_Tahun' },
-                    { data: 'KE_IDSemester', name: 'KE_IDSemester' },
-                    { data: 'KE_Kelas', name: 'KE_Kelas' },
-                    { data: 'KE_DayaTampung', name: 'KE_DayaTampung' },
-                    { data: 'KE_PE_NIPPengajar', name: 'KE_PE_NIPPengajar' },
-                    { data: 'KE_Terisi', name: 'KE_Terisi' },
-                    { data: 'KE_Jadwal_IDHari', name: 'KE_Jadwal_IDHari' },
-                    { data: 'KE_Jadwal_JamMulai', name: 'KE_Jadwal_JamMulai' },
-                    { data: 'KE_Jadwal_JamUsai', name: 'KE_Jadwal_JamUsai' },
-                    { data: 'KE_Jadwal_Ruangan', name: 'KE_Jadwal_Ruangan' },
-                    { data: 'KE_KodeJurusan', name: 'KE_KodeJurusan' },
-                    { data: 'action', name: 'action' }
-                ]
-            });
-        });
-    </script>
-@endpush
+{{--@push('scripts')--}}
+{{--    <script>--}}
+{{--        $(function() {--}}
+{{--            $('#users-table').DataTable({--}}
+{{--                "scrollX": true,--}}
+{{--                processing: true,--}}
+{{--                serverSide: true,--}}
+{{--                ajax: '/kelas/json', //DIGANTI SESUAI CONTROLLER--}}
+{{--                columns: [--}}
+{{--                    { data: 'KE_ID', name: 'KE_ID' },--}}
+{{--                    { data: 'KE_KR_MK_ID', name: 'KE_KR_MK_ID' },--}}
+{{--                    { data: 'KE_Tahun', name: 'KE_Tahun' },--}}
+{{--                    { data: 'KE_IDSemester', name: 'KE_IDSemester' },--}}
+{{--                    { data: 'KE_Kelas', name: 'KE_Kelas' },--}}
+{{--                    { data: 'KE_DayaTampung', name: 'KE_DayaTampung' },--}}
+{{--                    { data: 'KE_PE_NIPPengajar', name: 'KE_PE_NIPPengajar' },--}}
+{{--                    { data: 'KE_Terisi', name: 'KE_Terisi' },--}}
+{{--                    { data: 'KE_Jadwal_IDHari', name: 'KE_Jadwal_IDHari' },--}}
+{{--                    { data: 'KE_Jadwal_JamMulai', name: 'KE_Jadwal_JamMulai' },--}}
+{{--                    { data: 'KE_Jadwal_JamUsai', name: 'KE_Jadwal_JamUsai' },--}}
+{{--                    { data: 'KE_Jadwal_Ruangan', name: 'KE_Jadwal_Ruangan' },--}}
+{{--                    { data: 'KE_KodeJurusan', name: 'KE_KodeJurusan' },--}}
+{{--                    { data: 'action', name: 'action' }--}}
+{{--                ]--}}
+{{--            });--}}
+{{--        });--}}
+{{--    </script>--}}
+{{--@endpush--}}
